c587e2b29079ac788510e55dba6dfa21b45f83b1
MeNoSmartBrain <ya.hemmer@gmail.com>
2022-01-01 23:27:29 +0100

    TCM 01.01.2021 work

--- documentation/bericht.for
+++ documentation/bericht.for
@@ -1,18 +1,18 @@
 \defcounter {refsection}{0}\relax 
-\contentsline {eq}{(\ref {eq-log}) Normaler Logarithmus}{9}{equation.1.2.1}%
+\contentsline {eq}{(\ref {eq-log}) Normaler Logarithmus}{11}{equation.1.2.1}%
 \defcounter {refsection}{0}\relax 
-\contentsline {eq}{(\ref {eq-dis-log}) Diskreter Logarithmus}{9}{equation.1.2.2}%
+\contentsline {eq}{(\ref {eq-dis-log}) Diskreter Logarithmus}{11}{equation.1.2.2}%
 \defcounter {refsection}{0}\relax 
-\contentsline {eq}{(\ref {eq-dis-exp}) Diskretere Exponentialfunktion}{10}{equation.1.2.3}%
+\contentsline {eq}{(\ref {eq-dis-exp}) Diskretere Exponentialfunktion}{11}{equation.1.2.3}%
 \defcounter {refsection}{0}\relax 
-\contentsline {eq}{(\ref {eq-factor}) Faktorisierung großer Zahlen}{10}{equation.1.2.4}%
+\contentsline {eq}{(\ref {eq-factor}) Faktorisierung großer Zahlen}{11}{equation.1.2.4}%
 \defcounter {refsection}{0}\relax 
-\contentsline {eq}{(\ref {eq-dis-exp-lN}) Diskretere Exponentialfunktion mit großen Zahlen}{10}{equation.1.2.5}%
+\contentsline {eq}{(\ref {eq-dis-exp-lN}) Diskretere Exponentialfunktion mit großen Zahlen}{12}{equation.1.2.5}%
 \defcounter {refsection}{0}\relax 
-\contentsline {eq}{(\ref {eq-dis-exp-bspOne}) Diskretere Exponentialfunktion mit großen Zahlen Beispiel-Eins}{10}{equation.1.2.6}%
+\contentsline {eq}{(\ref {eq-dis-exp-bspOne}) Diskretere Exponentialfunktion mit großen Zahlen Beispiel-Eins}{12}{equation.1.2.6}%
 \defcounter {refsection}{0}\relax 
-\contentsline {eq}{(\ref {eq-dis-exp-numSpace}) Diskretere Exponentialfunktion in Zahlenraum}{10}{equation.1.2.7}%
+\contentsline {eq}{(\ref {eq-dis-exp-numSpace}) Diskretere Exponentialfunktion in Zahlenraum}{12}{equation.1.2.7}%
 \defcounter {refsection}{0}\relax 
-\contentsline {eq}{(\ref {eq-dis-exp-bspTwo}) Diskretere Exponentialfunktion mit großen Zahlen Beispiel-Zwei}{11}{equation.1.2.8}%
+\contentsline {eq}{(\ref {eq-dis-exp-bspTwo}) Diskretere Exponentialfunktion mit großen Zahlen Beispiel-Zwei}{12}{equation.1.2.8}%
 \defcounter {refsection}{0}\relax 
-\contentsline {eq}{(\ref {eq-dis-exp-bspThree}) Diskretere Exponentialfunktion mit großen Zahlen Beispiel-Drei}{11}{equation.1.2.9}%
+\contentsline {eq}{(\ref {eq-dis-exp-bspThree}) Diskretere Exponentialfunktion mit großen Zahlen Beispiel-Drei}{12}{equation.1.2.9}%
